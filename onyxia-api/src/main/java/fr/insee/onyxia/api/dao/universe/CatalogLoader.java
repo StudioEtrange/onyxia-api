87b4535851ce055c08ea2cfd0e245a5db745d89d
Sylvain Boucault <sylvain.boucault_externe@rte-france.com>
2025-05-10 06:13:44 +0200

    fix log updateCatalog

--- onyxia-api/src/main/java/fr/insee/onyxia/api/dao/universe/CatalogLoader.java
+++ onyxia-api/src/main/java/fr/insee/onyxia/api/dao/universe/CatalogLoader.java
@@ -58,7 +58,7 @@ public class CatalogLoader {
     }
 
     public void updateCatalog(CatalogWrapper cw) {
-        LOGGER.info("updating catalog with id :{} and type {}", cw.getId(), cw.getType());
+        LOGGER.info("updating catalog with id :{} and type {} from {} with {} for skip tls verification", cw.getId(), cw.getLocation(), cw.getType(), Strcw.getSkipTlsVerify());
         if (cw.getType().equals(Repository.TYPE_HELM)) {
             updateHelmRepository(cw);
         } else {
